a1f189b74f78da02639c282781dd0d9bd867b4b3
abhinav3254 <abhinav3254@gmail.com>
2023-09-16 17:25:47 +0530

    some things fixed

--- src/app/components/Book/place-order/place-order.component.ts
+++ src/app/components/Book/place-order/place-order.component.ts
@@ -11,45 +11,16 @@ export class PlaceOrderComponent implements OnInit {
   constructor(private bookUtilityService: BookutilityService
   ) { }
 
-  orders: Root2[] = [];
+  orders: Order[] = [];
 
   ngOnInit(): void {
     this.bookUtilityService.getAllOrders().subscribe((res) => {
-      this.orders = res as Root2[];
+      this.orders = res as Order[];
       console.log(res);
-
     });
   }
 
-  // for rating
-  // postRating(myFormValue: any, bookId: any) {
-  //   // console.log(JSON.stringify(myFormValue.value.rating) + "<<<<<<<<<--------form value and bookId --> >>>>>>>>>>>   " + bookId);
-  //   const values = {
-  //     rating: '',
-  //     bookId: '',
-  //     comment: ''
-  //   }
 
-  //   // values.rating = JSON.stringify(myFormValue.value.rating);
-  //   values.bookId = JSON.stringify(bookId);
-  //   values.rating = JSON.stringify(parseInt(JSON.stringify(myFormValue.value.rating).replace(/"/g, ''), 10));
-  //   values.comment = myFormValue.value.comment;
-
-  //   console.log('values-------->' + JSON.stringify(values));
-
-  //   this.bookUtilityService.postRating(values).subscribe((res) => {
-  //   }, error => { // second parameter is to listen for error
-  //     console.log(error);
-  //     console.log(error.status);
-  //     if (error.status == 200) {
-  //       alert("Thanks For your Feedback");
-  //     } else if (error.status == 400) {
-  //       alert("You Already Rated");
-  //     } else {
-  //       alert("SOMETHING WENT WRONG")
-  //     }
-  //   });
-  // }
 
   postRating(myFormValue: any, bookId: any) {
     console.log('Form Value:', myFormValue); // Add this line for debugging
@@ -82,86 +53,121 @@ export class PlaceOrderComponent implements OnInit {
     });
   }
 
-
 }
 
-// export type Root = Root2[]
 
-export interface Root2 {
-  id: number
-  cart: Cart
-  user: User
-  payment: Payment
-  address: string
-  orderDate: string
+export interface Order {
+  id: number;
+  cart: Cart;
+  user: User;
+  payment: Payment;
+  address: string;
+  orderDate: Date;
 }
 
 export interface Cart {
-  id: number
-  cartItems: CartItem[]
-  user: any
+  id: number;
+  cartItems: CartItem[];
+  user: null;
 }
 
 export interface CartItem {
-  id: number
-  user: any
-  book: Book
-  bookPrice: number
-  quantity: number
-  cgst: number
-  sgst: number
-  discount: number
-  finalPrice: number
+  id: number;
+  user: null;
+  book: Book;
+  bookPrice: number;
+  quantity: number;
+  cgst: number;
+  sgst: number;
+  discount: number;
+  finalPrice: number;
 }
 
 export interface Book {
-  id: number
-  publishers: Publishers
-  author: Author
-  imageUrl: string
-  isbn: string
-  pageCount: string
-  bookStatus: boolean
-  publishDate: string
-  title: string
-  price: string
-  description: string
-  category: string
-}
-
-export interface Publishers {
-  id: number
-  publisherName: string
-  country: string
+  id: number;
+  publishers: Publishers;
+  author: Author;
+  imageUrl: string;
+  isbn: string;
+  pageCount: string;
+  bookStatus: boolean;
+  publishDate: Date;
+  title: string;
+  price: string;
+  description: string;
+  category: string;
+  averageRating: number;
+  listRatings: ListRating[];
+  bookQuantity: number;
 }
 
 export interface Author {
-  id: number
-  authorName: string
-  dateOfBirth: string
+  id: number;
+  authorName: string;
+  dateOfBirth: Date;
+  nationality: null;
+  gender: null;
+}
+
+export interface ListRating {
+  id: number;
+  rating: number;
+  user: User;
+  ratingPostDate: Date | null;
+  comment: null | string;
 }
 
 export interface User {
-  id: number
-  username: string
-  name: string
-  email: string
-  password: string
-  phone: string
-  address: string
-  status: string
-  role: string
+  id: number;
+  username: Username;
+  name: Email;
+  email: Email;
+  password: string;
+  phone: string;
+  address: Address;
+  status: string;
+  role: Role;
+  dateOfBirth: Date;
+  gender: Gender;
+  registerDate: Date;
+  lastLogin: Date;
+}
+
+export enum Address {
+  Sector2ShantiNiketan = "sector 2 shanti niketan",
+}
+
+export enum Email {
+  Abhinav3254GmailCOM = "abhinav3254@gmail.com",
+}
+
+export enum Gender {
+  Male = "male",
+}
+
+export enum Role {
+  Admin = "admin",
+  User = "user",
+}
+
+export enum Username {
+  Rahul1 = "rahul1",
+  Root = "root",
+}
+
+export interface Publishers {
+  id: number;
+  publisherName: string;
+  country: string;
 }
 
 export interface Payment {
-  pid: number
-  user: any
-  cardNumber: string
-  bankName: string
-  nameOnCard: string
-  cvv: string
-  expiryDate: string
-  amount: number
+  pid: number;
+  user: null;
+  cardNumber: string;
+  bankName: string;
+  nameOnCard: string;
+  cvv: string;
+  expiryDate: Date;
+  amount: number;
 }
-
-
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -81,6 +81,7 @@ export interface Book {
   description: string
   category: string
   averageRating: number
+  bookQuantity: number
   listRatings: ListRating[]
 }
 
