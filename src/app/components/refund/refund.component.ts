45442374a86ecc868b4aa418858f8bc20c3380fc
abhinav3254 <abhinav3254@gmail.com>
2023-09-16 20:39:35 +0530

    refund generated

--- src/app/components/refund/refund.component.ts
+++ src/app/components/refund/refund.component.ts
@@ -8,14 +8,51 @@ import { BookutilityService } from 'src/app/services/bookutility.service';
 })
 export class RefundComponent implements OnInit {
 
-  refund: any;
+  refunds: Orders[] = [];
 
   constructor(private bookService: BookutilityService) { }
   ngOnInit(): void {
     this.bookService.getRefund().subscribe((res) => {
       console.log(res);
-      this.refund = res;
+      this.refunds = res as Orders[];
     });
   }
 
 }
+
+export interface Orders {
+  id: number;
+  cart: null;
+  user: User;
+  payment: Payment;
+  address: string;
+  orderDate: Date;
+}
+
+export interface Payment {
+  pid: number;
+  user: null;
+  cardNumber: string;
+  bankName: string;
+  nameOnCard: string;
+  cvv: string;
+  expiryDate: Date;
+  amount: number;
+}
+
+export interface User {
+  id: number;
+  username: string;
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  address: string;
+  status: string;
+  role: string;
+  dateOfBirth: Date;
+  gender: string;
+  registerDate: Date;
+  lastLogin: Date;
+}
+
