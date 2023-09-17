5a6fc427b7141294970f517af95ef6cc7f202b0e
abhinav3254 <abhinav3254@gmail.com>
2023-09-17 20:58:10 +0530

    fixing things

--- src/app/components/update-profile/update-profile.component.ts
+++ src/app/components/update-profile/update-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 
@@ -7,13 +7,52 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './update-profile.component.html',
   styleUrls: ['./update-profile.component.css']
 })
-export class UpdateProfileComponent {
+export class UpdateProfileComponent implements OnInit {
+
+  user: Root = {
+    id: 0,
+    username: '',
+    name: '',
+    email: '',
+    password: '',
+    phone: '',
+    address: '',
+    status: '',
+    role: '',
+    gender: '',
+    registerDate: '',
+    lastLogin: '',
+    dateOfBirth: ''
+  };
+
 
   constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUserDetails().subscribe((res) => {
+      console.log(res);
+      this.user = res as Root;
+    });
+  }
 
   formSubmit(myForm: NgForm) {
     this.userService.updateUserDetails(myForm.value).subscribe();
   }
 }
 
+export interface Root {
+  id: number
+  username: string
+  name: string
+  email: string
+  password: string
+  phone: string
+  address: string
+  status: string
+  role: string
+  gender: string,
+  registerDate: string,
+  lastLogin: string,
+  dateOfBirth: string
+}
 
